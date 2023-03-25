6dc2dee24cbca50baa961fe417aa9b4d1d3b5d01
Rafieaydin <rafie.aydin1@gmail.com>
2023-03-25 14:29:58 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const click = () => {
+//     console.log("clicked")
+//     // fetch('https://ipapi.co/json/')
+//     // .then((response)=>{response.json()})
+//     // .then((data)=>{
+//     //     data.latitude = parseFloat(data.latitude)
+//     //     data.longitude = parseFloat(data.longitude)
+//     //     console.log(data)
+//     // })
+// }
+
